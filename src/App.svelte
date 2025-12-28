0cbaf4c5a05ee82787b1916e6c706af239f64aa7
Kaytster <ktsharps@gmail.com>
2025-12-28 22:15:53 +0000

    added a grid for the colour picker and image to display next to eachother

--- src/App.svelte
+++ src/App.svelte
@@ -11,4 +11,19 @@
     <p>This webtool is designed to make identifying colours in an image easier.</p>
     <p class="text-sm">To get started upload an image, and choose a colour.</p>
   </article>
+
+  <br />
+
+  <article class="uploadCard">
+    <p>Select an image from your device</p>
+  </article>
+
+  <br />
+
+  <div class="container">
+    <div class="grid">
+      <article class="picker">1</article>
+      <article class="image">2</article>
+    </div>
+  </div>
 </main>
