1a7247654ac33655dcebd79b86b70b618178244f
Kaytster <ktsharps@gmail.com>
2026-01-09 11:42:39 +0000

    Fixed errors, created tsconfig, and made sure the runes are being recognised

--- src/lib/Eyedropper.svelte
+++ src/lib/Eyedropper.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
+    interface EyeDropper {
+        new (): EyeDropper;
+        open(options?: {signal: AbortSignal}): Promise<{sRGBHex: string}>;
+    }
+
+    const _EyeDropper = (window as any).EyeDropper as EyeDropper;
+
     let {colour = $bindable()} = $props();
 
     let errorMessage = $state("");
 
     async function openEyedropper() {
         errorMessage = "";
-        if (!window.EyeDropper) {
+        if (!_EyeDropper) {
             errorMessage = "The Eyedropper is not supported by your browser. Please use the HEX Codes or Colour Wheel." 
             return;       
         }
 
-        const eyeDropper = new EyeDropper();
+        const eyeDropper = new _EyeDropper();
 
         try {
             const result = await eyeDropper.open();
