50a71f6c67d4849533d4184edf707236053f0930
Kaytster <ktsharps@gmail.com>
2026-01-09 11:31:25 +0000

    initial setup using svelte runes

--- src/lib/Eyedropper.svelte
+++ src/lib/Eyedropper.svelte
@@ -1,3 +1,22 @@
 <script lang="ts">
-    
+    let {colour = $bindable()} = $props();
+
+    let errorMessage = $state("");
+
+    async function openEyedropper() {
+        errorMessage = "";
+        if (!window.EyeDropper) {
+            errorMessage = "The Eyedropper is not supported by your browser. Please use the HEX Codes or Colour Wheel." 
+            return;       
+        }
+
+        const eyeDropper = new EyeDropper();
+
+        try {
+            const result = await eyeDropper.open();
+            colour = result.sRGBHex;
+        } catch (e) {
+            console.log("Eyedropper closed")
+        }
+    }
 </script>
